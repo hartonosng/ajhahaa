cde878138b2f3c69e335004ce157143e4ec096b2
hartonosng <52649900+hartonosng@users.noreply.github.com>
2024-08-07 05:43:20 +0700

    Update Hh.pyj

--- Hh.py
+++ Hh.py
@@ -1,33 +1,33 @@
-WITH json_parts AS (
+WITH product_json AS (
     SELECT
-        customer_id,
+        goal_id,
         product_type,
-        CONCAT('{"account_id":"', account_id, '","current_balance":', current_balance, '}') AS account_json
+        CONCAT('{"accountid":"', accountid, '","current_balance":', current_balance, '}') AS account_json
     FROM accounts
 ),
 
 json_array AS (
     SELECT
-        customer_id,
+        goal_id,
         product_type,
-        CONCAT('[', GROUP_CONCAT(account_json, ','), ']') AS accounts_json
-    FROM json_parts
-    GROUP BY customer_id, product_type
+        CONCAT('[', CONCAT_WS(',', collect_list(account_json)), ']') AS accounts_json
+    FROM product_json
+    GROUP BY goal_id, product_type
 ),
 
 json_object AS (
     SELECT
-        customer_id,
-        CONCAT('{"', product_type, '":', accounts_json, '}') AS product_json
+        goal_id,
+        CONCAT('\"', product_type, '\":', accounts_json) AS product_json
     FROM json_array
 ),
 
 final_json AS (
     SELECT
-        customer_id,
-        CONCAT('{', GROUP_CONCAT(product_json, ','), '}') AS account_json
+        goal_id,
+        CONCAT('{', CONCAT_WS(',', collect_list(product_json)), '}') AS account_json
     FROM json_object
-    GROUP BY customer_id
+    GROUP BY goal_id
 )
 
-SELECT customer_id, account_json FROM final_json;
+SELECT goal_id, account_json FROM final_json;
